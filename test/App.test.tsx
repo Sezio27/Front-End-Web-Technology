cdf88d8e335bbdf2a00a1aa50cce58a7f3cf2d95
Thorsager1 <s195470@student.dtu.dk>
2023-03-13 16:15:14 +0100

    div

--- test/App.test.tsx
+++ test/App.test.tsx
@@ -1,15 +1,38 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
 import { describe, expect, it } from "vitest";
+import userEvent from "@testing-library/user-event";
+import { debug } from "vitest-preview";
 import App from ".././src/App";
 
 describe(App.name, () => {
     it("should render", async () => {
       render(<App />);
-
     
       const test = await screen.findAllByText("Cart");
 
       expect(test).toHaveLength(1);
     });
   });
+
+
+describe(App.name, () => {
+  it("should get the right price", async () => {
+    const user = userEvent.setup();
+
+    render(<App />);
+
+    const apples = screen.getByRole("row", {name: "TEST Apples Buy 5 more to save 20%! 0 0 DKK"});
+
+    const quantityPicker = apples.getElementsByClassName("quantityContainer")[1];
+
+    await user.type(quantityPicker, "10");
+
+    await user.type(quantityPicker, '{enter}');
+
+    debug();
+
+    await screen.findByText("20 DKK");
+    
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
+    css: true,
     environment: "jsdom",
     setupFiles: ["vitest.setup.ts"],
   },
