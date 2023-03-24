3baa433e0518e9f59fa420b706f6883e3c2ce01a
lillebrian <s195456@student.dtu.dk>
2023-03-24 16:55:34 +0100

    Working icon and loading from request bin

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,10 +1,17 @@
 import Basket from "./Basket/Basket";
 import Checkout from "./Checkout/Checkout";
 import "./Cart.css";
+import "../../css/LoadingLogo.css"
+import reload from "../../assets/reload.png"
 import { calculateItemDiscount } from "../../Utilities/SavingsUtility";
 import { useCartContext } from "../../contexts/CartContext";
 import { useEffect, useState } from "react";
 import { fetchProductList } from "../Utility/fetchProducts";
+
+// Custom wait method to showcase icon spinning
+const delay = (ms: number) => new Promise(
+  resolve => setTimeout(resolve, ms)
+);
 
 const Cart = () => {
   const { basketItems, setBasketItems } = useCartContext();
@@ -12,17 +19,21 @@ const Cart = () => {
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
+    // Fetching items from custom build method. 
     const fetchItems = async () => {
       const items = await fetchProductList()
       setBasketItems(items)
+      await delay(3000)
       setLoading(false)
     }
     fetchItems()
   }, []);
 
   return (
+    
     <div className="cartContainer">
-      {!loading && basketItems.length > 0 ? (
+      {loading ? <img src={reload} className="loading-logo"/> : 
+      (basketItems.length > 0 ? (
         <div className="cartSection">
           <div className="basketContainer">
             <Basket />
@@ -33,9 +44,9 @@ const Cart = () => {
         </div>
       ) : (
         <span>Your basket is empty.</span>
-      )}
+      ))}
     </div>
-  );
+  )
 };
 
 export default Cart;
