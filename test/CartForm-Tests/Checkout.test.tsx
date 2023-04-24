4a7e152ab748dafe032b9f7101731cc57ee37ae8
lillebrian <s195456@student.dtu.dk>
2023-04-24 12:19:04 +0200

    modifying tests

---
+++ test/CartForm-Tests/Checkout.test.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { afterEach, describe, expect, it, vi, vitest } from "vitest";
+
+import App from "../../src/App";
+
+
+describe(App.name, () => {
+   it("Should render checkout box", async () => {
+      render(<App/>)
+      
+      
+      
+   })
+})
---
+++ test/CheckoutForm-Tests/Form.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen, act } from "@testing-library/react";
+import React from "react";
+import { afterEach, describe, expect, it, vi, vitest } from "vitest";
+import userEvent from "@testing-library/user-event";
+import App from "../../src/App";
+import CheckoutForm from "../../src/components/Forms/CheckoutForm";
+import { useNavigate } from "react-router-dom";
+//import mockResponse from ".././src/data/mock-response.json"
+//import { debug } from "vitest-preview"; 
+
+describe(App.name, () => {
+  it("should render", async () => {
+    render(<App />);
+    
+    const test = await screen.findAllByText("Cart");
+
+    expect(test).toHaveLength(1);
+  });
+});
