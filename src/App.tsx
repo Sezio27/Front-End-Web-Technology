4dc013663c7d15bba90fbba4f3e84ffefe5b6796
IntelliLakshmi <91959080+IntelliLakshmi@users.noreply.github.com>
2023-04-28 16:50:35 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,47 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css"; 
+import Cart from "./components/Cart/Cart";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import CheckoutForm from "./components/Forms/CheckoutForm";
+import { CartProvider } from "./contexts/CartContext";
+import Payment from "./components/Payment/Payment";
+import Receipt from "./components/Receipt/Receipt";
+import products from "./data/products.json";
+import { Item } from "./Types/Types";
+import {useState, useEffect} from "react"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+function App() {
+
+ 
+  return (
+    <CartProvider>
+      <BrowserRouter>
+        <Routes>
+            <Route
+              path="/"
+              element={
+                <div className="App">
+                  <div className="siteContainer">
+                    <div className="siteSection">
+                      <h1 className="Title"> Checkout Flow </h1>
+                    </div>
+                    <div className="siteSection">
+                      <h1 className="Subtitle"> Cart </h1>
+                    </div>
+                    <div className="siteSectionComponent">
+                        <Cart/>
+                    </div>
+                  </div>
+                </div>
+              }
+            />
+            
+            <Route path="/Checkout" element={<CheckoutForm />} />
+            <Route path="/Checkout/Payment" element={<Payment/>} />
+            <Route path="/Checkout/Receipt" element={<Receipt/>} />
+            
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
+  );
+}
+
+export default App;
