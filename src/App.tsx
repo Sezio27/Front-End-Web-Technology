aa6216e311aba621097e163142617bdfdc8b5247
IntelliLakshmi <91959080+IntelliLakshmi@users.noreply.github.com>
2023-04-28 16:46:08 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,14 @@
-import Cart from "./components/Cart/Cart";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import CheckoutForm from "./components/Forms/CheckoutForm";
-import { CartProvider } from "./contexts/CartContext";
-import Payment from "./components/Payment/Payment";
-import products from "./data/products.json";
-import { Item } from "./Types/Types";
-import {useState, useEffect} from "react"
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css"; 
 
-function App() {
-
- 
-  return (
-    <CartProvider>
-      <BrowserRouter>
-        <Routes>
-            <Route
-              path="/"
-              element={
-                <div className="App">
-                  <div className="siteContainer">
-                    <div className="siteSection">
-                      <h1 className="Title"> Checkout Flow </h1>
-                    </div>
-                    <div className="siteSection">
-                      <h1 className="Subtitle"> Cart </h1>
-                    </div>
-                    <div className="siteSectionComponent">
-                        <Cart/>
-                    </div>
-                  </div>
-                </div>
-              }
-            />
-            
-            <Route path="/Checkout" element={<CheckoutForm />} />
-            <Route path="/Checkout/Payment" element={<Payment/>} />
-            
-        </Routes>
-      </BrowserRouter>
-    </CartProvider>
-  );
-}
-
-export default App;
-
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
