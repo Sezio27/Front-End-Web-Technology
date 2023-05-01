8ea75bb1d007703bf426f7e60e61ba022dc21f6b
Sezio27 <sezio27@gmail.com>
2023-05-01 15:07:03 +0200

    Updated checkoutResponsive similiar to checkout + added logic for displaying total price

--- src/components/Cart/Checkout/CheckoutResponsive.tsx
+++ src/components/Cart/Checkout/CheckoutResponsive.tsx
@@ -1,84 +1,106 @@
 import "./Checkout.css";
 import shoppingLogo from "../../../assets/shoppingbag.png"
-import { RoundToNearestHalf, TwoDecimals } from "../../../Utilities/NumberUtitlity";
+import { TwoDecimals } from "../../../Utilities/NumberUtitlity";
 import LockIcon from "../../../assets/lockicon.png";
 import { useCartContext } from "../../../contexts/CartContext";
 import Popup from "reactjs-popup";
 import { handleNavigation } from "../../../Router";
 
 interface ICheckout {
-  currency: string;
+    currency: string;
 }
 
 const CheckoutResponsive = ({ currency }: ICheckout) => {
-  const {calculateTotals} = useCartContext()
+    const { calculateTotals } = useCartContext()
 
-  const {totalQuantity, totalPrice, totalSavings, totalDiscountActive} = calculateTotals()
-  
-  const promtSavings = () => {
-    if (totalPrice == undefined) return; 
-    return totalDiscountActive
-    ? `You've shaved an extra ${TwoDecimals((totalPrice * 1.1) - totalPrice)} (10%) off your total order!`
-      : `Buy for ${300 - totalPrice},- more to save 10%`;
-  };
-  
+    const { totalQuantity, totalPrice, totalRebateSavings, totalOrderSavings, totalPriceAfterSavings, totalDiscountActive } = calculateTotals()
 
-  return (
-    <div className="menuContainer">
-        <div className="menuContainerItem">
-            <span className="itemCounter">{totalQuantity}</span>
-            <Popup
+    const promtSavings = () => {
+        if (totalPrice == undefined) return;
+        return totalDiscountActive
+            ? `You've saved 10% off your total order!`
+            : `Buy for ${300 - totalPrice},- more to save 10%`;
+    };
+
+    const handleCheckout = () => {
+        localStorage.setItem("totalPrice", String(totalPrice));
+        handleNavigation("/checkout");
+    };
+
+
+    return (
+        <div className="menuContainer">
+            <div className="menuContainerItem">
+                <span className="itemCounter">{totalQuantity}</span>
+                <Popup
                     trigger={
-                            <img src={shoppingLogo} alt="shopping cart" style={{
-                                height: 50,
-                                width: 50,
-                            }}/>}
+                        <img src={shoppingLogo} alt="shopping cart" style={{
+                            height: 50,
+                            width: 50,
+                        }} />}
                     modal
                     nested
-                    
+
                 >{close => {
-                return (
-                    <div className="modal">
-                        <button className="close" onClick={close}>
-                            &times;
-                        </button>
-                        <div className="header"> Checkout</div>
-                        <div className="content">
-                            <div className="primaryContainer">
-                                <div className="textSection">
-                                    Items in cart:
-                                    <div className="textEnd">{totalQuantity}</div>
-                                </div>
-                                <div className="textSection">
-                                    Total price of items:
-                                    <div className="textEnd">{`${TwoDecimals(totalPrice)},- ${currency}`}</div>
-                                </div>
-                                {totalSavings > 0 && (
-                                    <div className="savingsText">
-                                        <span>Price savings of items</span>
-                                        <div className="savingsTextEnd">{`${totalSavings},-`}</div>
+                    return (
+                        <div className="modal">
+                            <button className="close" onClick={close}>
+                                &times;
+                            </button>
+                            <div className="header"> Checkout</div>
+                            <div className="content">
+                                <div className="primaryContainer">
+                                    <div className="textSection">
+                                        Items in cart:
+                                        <div className="textEnd">{totalQuantity}</div>
                                     </div>
-                                )}
-                                <div>
-                                    <div className="totalDiscountText"> {promtSavings()} </div>
-                                 
-                                        <button className="checkoutButton" onClick={() => handleNavigation('/checkout')}>
+                                    {totalPriceAfterSavings > 0 && (
+                                        <div className="textSection total">
+                                            Total price of items:
+                                            <div className="textEnd" data-testid='totalPrice'>{`${TwoDecimals(totalPrice)},- ${currency}`}</div>
+                                        </div>
+                                    )
+                                    }
+                                    {totalRebateSavings > 0 && (
+                                        <div className="savingsText">
+                                            <span>Price savings of items</span>
+                                            <div className="savingsTextEnd">{`- ${TwoDecimals(totalRebateSavings)},-`}</div>
+                                        </div>
+                                    )}
+                                    {totalRebateSavings > 0 && (
+                                        <div className="savingsText">
+                                            <span>Buying for more than 300</span>
+                                            <div className="savingsTextEnd">{`- ${TwoDecimals(totalOrderSavings)},-`}</div>
+                                        </div>
+                                    )}
+                                    <div className="textSection">
+                                        Total price:
+                                        {totalPriceAfterSavings > 0 ? (
+                                            <div className="textEnd totalAfter" data-testid='totalPriceAfter'>{`${TwoDecimals(totalPriceAfterSavings)},- ${currency}`}</div>
+                                        ) :
+                                            <div className="textEnd totalAfter" data-testid='totalPriceAfter'>{`${TwoDecimals(totalPrice)},- ${currency}`}</div>
+                                        }
+                                    </div>
+                                    <div>
+                                        <div className="totalDiscountText"> {promtSavings()} </div>
+
+                                        <button className="checkoutButton" onClick={() => handleCheckout()}>
                                             <div className="checkoutButtonInside">
-                                                <img src={LockIcon} className="checkoutButtonIcon"/>
+                                                <img src={LockIcon} className="checkoutButtonIcon" />
                                                 <span className="checkoutButtonText">Checkout</span>
                                             </div>
                                         </button>
-                      
+
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                );
-            }}
+                    );
+                }}
                 </Popup>
+            </div>
         </div>
-    </div>
-  );
+    );
 };
 
 export default CheckoutResponsive;
